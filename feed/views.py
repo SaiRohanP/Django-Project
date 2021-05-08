82ed6296259ba68f98f4b6b818177c7ca394ec50
Sai Rohan P <psairohan21@gmail.com>
2021-05-08 11:07:24 +0530

    PostDetailView

--- feed/views.py
+++ feed/views.py
@@ -1,13 +1,18 @@
 #(for function based view)from django.shortcuts import render
 
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from .models import Post
 
 # Create your views here.
 
 class HomePage(ListView):
-    http_method_names = ["get"]
+    http_method_names = ["get"]#default
     template_name = "feed/homepage.html"
     model = Post
     context_object_name = "posts"
     queryset = Post.objects.all().order_by('-id')[0:30]
+
+class PostDetailView(DetailView):
+    template_name = "feed/detail.html"
+    model = Post
+    context_object_name = "post"
