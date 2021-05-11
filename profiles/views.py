7d8d158256e9d8712570d8ef4b426229e5994c7e
Sai Rohan P <psairohan21@gmail.com>
2021-05-11 17:32:08 +0530

    Getting total posts

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import DetailView
 from django.contrib.auth.models import User
+from feed.models import Post
 # Create your views here.
 
 class ProfileDetailView(DetailView):
@@ -8,3 +9,10 @@ class ProfileDetailView(DetailView):
     context_object_name = "user"
     slug_field = "username"
     slug_url_kwarg = "username"
+
+    def get_context_data(self,**kwargs):
+        user = self.get_object()
+        context = super().get_context_data(**kwargs)
+        context['total_posts'] = Post.objects.filter(author=user).count()
+
+        return context
